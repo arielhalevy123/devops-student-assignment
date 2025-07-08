e796cd1deab60f6ce0f722d4cd5f009c6f4af8fa
arielhalevy123 <130355480+arielhalevy123@users.noreply.github.com>
2025-07-08 23:27:38 +0300

    Add GitHub Actions workflow for CDK deploy

---
+++ upload_files.py
@@ -0,0 +1,20 @@
+import boto3
+import os
+
+
+bucket_name = "devopsstudentassignmentstack-mybucketf68f3ff0-q6ibws3kjamw"
+
+
+s3 = boto3.client("s3")
+
+
+folder_path = "sample_files"
+
+
+for filename in os.listdir(folder_path):
+    file_path = os.path.join(folder_path, filename)
+    if os.path.isfile(file_path):
+        print(f"Uploading {filename}...")
+        s3.upload_file(file_path, bucket_name, filename)
+
+print("✅ All files uploaded successfully!")
