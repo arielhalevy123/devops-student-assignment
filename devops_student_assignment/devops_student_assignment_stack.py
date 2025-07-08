71a9c550a711964861b54c35df9d87f4ef94e513
arielhalevy123 <130355480+arielhalevy123@users.noreply.github.com>
2025-07-08 19:47:44 +0300

    Initial commit

--- devops_student_assignment/devops_student_assignment_stack.py
+++ devops_student_assignment/devops_student_assignment_stack.py
@@ -1,7 +1,7 @@
 from aws_cdk import (
-    # Duration,
     Stack,
-    # aws_sqs as sqs,
+    aws_s3 as s3,
+    RemovalPolicy,
 )
 from constructs import Construct
 
@@ -10,10 +10,8 @@ class DevopsStudentAssignmentStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
-
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "DevopsStudentAssignmentQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        bucket = s3.Bucket(self,
+                           "MyBucket",
+                           versioned=True,
+                           removal_policy=RemovalPolicy.DESTROY,
+                           auto_delete_objects=True)
